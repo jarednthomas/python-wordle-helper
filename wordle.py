5605afe81a97deaa2789c874d1c3edc2f559b07d
Jared Thomas <jared@lucentdigital.co>
2022-02-14 22:29:14 -0600

    init

---
+++ wordle.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+import argparse, re, string, sys
+
+
+def break_wordle():
+
+    # Init ArguementParser
+    parser = argparse.ArgumentParser()
+
+    # A string representing a wordle row, with # for uncertain placeholders
+    parser.add_argument("input_string", help="world row with # for unknowns")
+
+    # Includes are letters known to be in the word
+    parser.add_argument("-i", "--include", help="characters that are for sure included")
+
+    # Excludes are letters known to not be in the words
+    parser.add_argument("-e", "--exclude", help="characters that are for sure exluded")
+
+    # Additional args for verbose output and writing to file
+    parser.add_argument("-v", "--verbose", help="output all possible information", action="store_true")
+    parser.add_argument("-w", "--write", help="write to file at the current directory instead of echoing output", action="store_true")
+
+    # Parse Arguements 
+    args = parser.parse_args()
+
+
+    verbose = args.verbose
+    if verbose:
+        print("Arguments")
+        print(args, end="\n\n")
+        print("[GIVEN]: {}".format(args.input_string))
+
+    # Reject invalid lengths
+    if len(args.input_string) != 5:
+        sys.exit("Invalid number of characters: {}. Expected 5".format(len(args.input_string)))
+
+    validated = []
+    # Ensure given string has only expected chars
+    unvalidated_string = args.input_string
+    for char in unvalidated_string:
+
+        # If char is valid add to validated string as lowercase
+        if char in string.ascii_lowercase or char in string.ascii_uppercase or char == "#":
+            if char == "#":
+                validated.append(char)
+            else:
+                validated.append(char.lower())
+        else:
+            sys.exit("Invalid input: [{}]\nOnly letters and # for unknowns are allowed, eg: h#ll#".format(char))
+
+    # Generate valid string from validated
+    validated_string = "".join([i for i in validated])
+
+    # Reject valid strings of invalid lengths, double check
+    if len(validated_string) != 5:
+        sys.exit("Invalid length: {}\nExpected length of 5".format(len(validated_string)))
+
+    unfinished_regex = []
+    # Turn validated string into a regex string
+    for c in validated_string:
+        if c == "#":
+            unfinished_regex.append("[a-z]")
+        else:
+            unfinished_regex.append("[{}]".format(c))
+
+    wordle_regex = "".join([j for j in unfinished_regex])
+    if(wordle_regex): 
+        if args.verbose: print("[REGEX]: {}\n".format(wordle_regex))
+    else:
+        sys.exit("An error occured while trying to generate regex. Create an issue on github and post the --verbose output, please.\n{}".format(wordle_regex))
+
+    possible_words = []
+    # Iterate through dictionary of five letter words with regex
+    with open("./five-letter-words/sgb-words.txt") as f:
+        for line in f:
+            # If the line matches our regular expressions
+            if (re.findall(wordle_regex, line)):
+                possible_words.append(line)
+
+    excludes_parsed = []
+    # If passed chars to exclude, filter the words 
+    words_excluded = 0
+    if args.exclude:
+        for word in possible_words:
+            word_has_excludes = 0
+
+            for char in word:
+                for exclude in args.exclude:
+                    if char == exclude:
+                        word_has_excludes += 1 
+            if word_has_excludes == 0: 
+                excludes_parsed.append(word)
+            else:
+                words_excluded += 1
+
+        possible_words = excludes_parsed
+        
+    includes_parsed = []
+    # If passed chars that are known in the word
+    if args.include:
+        
+        # Iterate through all possible words
+        for word in possible_words:
+            has_includes = 0
+
+            # Iterate through every known letter, per possible word
+            for include in args.include:
+                if include in word:
+                    has_includes += 1
+
+            # If the word has the same number of includes, add it to the list
+            if has_includes == len(args.include):
+                includes_parsed.append(word)
+
+        # Update the list of possible words
+        possible_words = includes_parsed
+
+
+    # Sort the list
+    possible_words.sort()
+
+    # If there is a result echo it out or write to file
+    if len(possible_words) == 1:
+        for line in possible_words:
+            print("The Solution is:\n\n{}".format(line))
+            sys.exit()
+
+
+    if len(possible_words) != 0:
+        
+        if args.write:
+            print("Found: {}".format(len(possible_words)))
+            with open("./possible_words.txt", "w") as wf:
+                for word in possible_words:
+                    wf.write(word)
+        else:
+            [print(i, end="") for i in possible_words]
+
+            
+            if verbose:
+                print("Number of words excluded: {}".format(words_excluded))
+
+            print("Found: {}".format(len(possible_words)))
+            
+    else:
+        print("No Results")
+
+
+if __name__ == "__main__":
+    break_wordle()
